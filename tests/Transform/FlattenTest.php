dda94ecaf1c2f51ba6813c83686dc36fe48b44c6
Hédic Guibert <guiberthedic@gmail.com@gmail.com>
2022-12-13 12:21:10 +0100

    feature(flatten): download tests from W3C rather than committing them

--- tests/Transform/FlattenTest.php
+++ tests/Transform/FlattenTest.php
@@ -3,13 +3,9 @@
 namespace Tests\Transform;
 
 use Jolicode\JsonLd\Flatten\Flattener;
-use Symfony\Component\Finder\Finder;
 
 class FlattenTest extends AbstractTransformTest
 {
-    private const INPUT_FILES = self::FIXTURES_PATH . '/flatten/input';
-    private const OUTPUT_FILES = self::FIXTURES_PATH . '/flatten/output';
-
     /** @dataProvider provideInputsAndOutputs */
     public function testFlatten(string $jsonToFlatten, string $expected): never
     {
@@ -21,18 +17,21 @@ class FlattenTest extends AbstractTransformTest
 
     public function provideInputsAndOutputs(): iterable
     {
-        $inputFinder = new Finder();
-        $inputFinder->files()->in(self::INPUT_FILES);
+        foreach ($this->getInputFiles() as $inputFile) {
+            $outputFile = $this->getOutputFile(
+                preg_replace('/-in/', '-out', $inputFile->getFilename())
 
-        foreach ($inputFinder as $inputFile) {
-            $outputFile = preg_replace('/-in/', '-out', $inputFile->getFilename());
-            $expectedOutput = file_get_contents(sprintf(
-                '%s/%s',
-                self::OUTPUT_FILES,
-                $outputFile
-            ));
+            );
 
-            yield [$inputFile->getContents(), $expectedOutput];
+            yield [
+                $inputFile->getContents(),
+                file_get_contents($outputFile)
+            ];
         }
+    }
+
+    protected function getAlgorithmName(): string
+    {
+        return self::ALGO_FLATTEN;
     }
 }
